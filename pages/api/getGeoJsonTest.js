f2ba1f76e6519268a1d2f1cd0f74c29b39c7dac8
Clavvv <Schpeen@proton.me>
2023-03-27 15:44:17 -0700

    create database connection functionality, create some table components, chartjs exploration code

--- pages/api/getGeoJsonTest.js
+++ pages/api/getGeoJsonTest.js
@@ -1,25 +1,43 @@
-const { Pool }= require('pg');
-const config= require('private/config.json');
-const { stringify } = require('querystring');
 
-const pool= new Pool({
-    user: config.user,
-    password: config.password,
-    host: config.host,
-    database: config.database,
-    port: config.port,
-    idleTimeoutMillis: 30000,
-    connectiontimeoutMillis: 2000,
-    max: 10
-});
-
-pool
-    .query('SELECT * FROM alexrodhomerun;')
-    .then((res) => console.log(res.rows))
-    .catch((err) => console.error('Error Executing Query...eat shit', err.stack))
+export default function getGeoJsonTest(req, res) {
 
 
+    const { Pool } = require('pg');
+    const config = require('private/config.json');
+    const fs = require('fs');
 
+    const pool = new Pool({
+        user: config.database_configuration.user,
+        password: config.database_configuration.password,
+        host: config.database_configuration.host,
+        database: config.database_configuration.database,
+        port: config.database_configuration.port,
+        idleTimeoutMillis: 30000,
+        connectiontimeoutMillis: 2000,
+        max: 10
+    });
+
+    pool
+        .query('SELECT * FROM alexrodhomerun;')
+        .then((res) => {
+            const results = res.rows.map(row => {
+                return {
+                    "home_runs": row.HR,
+                    "year": row.Year
+                };
+            });
+            const json = JSON.stringify(results);
+            fs.writeFile('result.json', json, (err) => {
+                if (err) {
+                    console.error('Error Writing File', err.stack);
+                }
+                else {
+                    console.log('File Saved Successfully');
+                }
+            });
+        })
+        .catch((err) => console.error('Error Executing Query...', err.stack));
+}
 
 
 
--- pages/data.js
+++ pages/data.js
@@ -1,20 +1,54 @@
 import React from 'react'
 
 
-function MyButton() {
-    return (
-        <button>я гей</button>
-    );
-}
-
 
 export default function MyApp() {
-    return (
-        <div>
-            <h1>Welcome to my app</h1>
-            <MyButton/>
 
-            {console.log()}
+    const fetchDataPreview = async () => {
+        try {
+            const res= await fetch('/api/getGeoJsonTest')
+            const data= await res.json()
+            console.log(data)
+
+        } catch (error) {
+            console.error('Error', error)
+        }
+    }
+
+
+
+
+
+
+
+
+    return (
+        <div className='flex h-screen w-screen'>
+            <div className='h-full w-full'>
+                <button onClick= {fetchDataPreview}>Get Data</button>
+                <table className='mx-auto my-20 border border-slate-500 border-collapse'>
+                    <thead className='border border-separate border-slate-500'>
+                        <tr>
+                            <th className='border border-separate border-slate-500 p-2'>Company</th>
+                            <th className='border border-collapse border-slate-500 p-2'>Contact</th>
+                            <th className='border border-collapse border-slate-500 p-2'>Country</th>
+                        </tr>
+                    </thead>
+                    <tbody className='border border-separate border-slate-500'>
+                        <tr>
+                            <td className='border border-collapse border-slate-500 p-2'>Company A</td>
+                            <td className='border border-collapse border-slate-500 p-2'>Maria A</td>
+                            <td className='border border-collapse border-slate-500 p-2'>Germany</td>
+                        </tr>
+                        <tr>
+                            <td className='border border-collapse border-slate-500 p-2'>Company B</td>
+                            <td className='border border-collapse border-slate-500 p-2'>Francisco Chang</td>
+                            <td className='border border-collapse border-slate-500 p-2'>Mexico</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
         </div>
     );
 }
