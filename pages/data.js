8266dabcc7e00ee0942d3849fbd34e7bd484a52e
Clavvv <Schpeen@proton.me>
2023-04-06 15:07:58 -0700

    create prototype leaflet interactive map

--- pages/data.js
+++ pages/data.js
@@ -61,31 +61,7 @@ export default class Data extends React.Component {
             <div className='flex h-screen w-screen'>
                 <div className='h-full w-full'>
                     <div className='flex flex-col h-full col-span-6 justify-center bg-white'>
-                        <table className='mx-auto border border-slate-500 border-collapse'>
-                            <thead className='border border-separate border-slate-500'>
-                                <tr>
-                                    <th className='border border-separate border-slate-500 p-2'>Company</th>
-                                    <th className='border border-collapse border-slate-500 p-2'>Contact</th>
-                                    <th className='border border-collapse border-slate-500 p-2'>Country</th>
-                                </tr>
-                            </thead>
-                            <tbody className='border border-separate border-slate-500'>
-                                <tr>
-                                    <td className='border border-collapse border-slate-500 p-2'>Company A</td>
-                                    <td className='border border-collapse border-slate-500 p-2'>Maria A</td>
-                                    <td className='border border-collapse border-slate-500 p-2'>Germany</td>
-                                </tr>
-                                <tr>
-                                    <td className='border border-collapse border-slate-500 p-2'>Company B</td>
-                                    <td className='border border-collapse border-slate-500 p-2'>Francisco Chang</td>
-                                    <td className='border border-collapse border-slate-500 p-2'>Mexico</td>
-                                </tr>
-                            </tbody>
-                        </table>
-
-                        <button className='m-2 p-1 self-center justify-center rounded border-2 border-black bg-white'>Show Graph</button>
-
-                        <div>
+                        <div className='justify-center place-items-center'>
                             <BarChart chartData={this.state.data}/>
                         </div>
 
